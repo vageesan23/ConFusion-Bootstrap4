3a1af82e122ee54ece99658b1191f67a77d6d6b3
Vageesan <vageesan.p@omobio.net>
2021-01-22 14:25:41 +0530

    Updated the files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,40 @@
 'use strict';
 
 module.exports = function (grunt) {
+
+  require('time-grunt')(grunt);
+  require('jit-grunt')(grunt);
   // Define the configuration for all the tasks
   grunt.initConfig({
-
+    sass: {
+        dist: {
+            files: {
+                'css/styles.css': 'css/styles.scss'
+            }
+        }
+    },
+    watch: {
+        files: 'css/*scss',
+        tasks:['sass']
+    },
+    browserSync: {
+        dev: {
+            bsFiles: {
+                src: [
+                    'css/*.css',
+                    '*.html',
+                    'js/*.js'
+                ]
+            },
+            options: {
+                watchTask: true,
+                server: {
+                    baseDir: './'
+                }
+            }
+        }
+    }
   });
+  grunt.registerTask('css', ['sass']);
+  grunt.registerTask('default', ['browserSync', 'watch']);
 };
